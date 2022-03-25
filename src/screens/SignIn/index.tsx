7cdc84069104da90849c2e213f6198e62a2b4382
Eduardo Marques <dado.nep@gmail.com>
2022-03-25 12:14:02 -0300

    initial commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+
+export function SignIn() {
+  return (
+    <Container>
+      <Text>SignIn</Text>
+    </Container>
+  );
+};
